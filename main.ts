ce8af0b2c05d58b556ab62b637fdb4e02633c8b5
john-holland-teals <146885148+john-holland-teals@users.noreply.github.com>
2023-10-10 14:15:51 -0400

    change duck to mario

--- main.ts
+++ main.ts
@@ -22,22 +22,30 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 let message = ""
 let locatorSprite: Sprite = null
 let mySprite = sprites.create(img`
-    . . . . . . . . . b 5 b . . . . 
-    . . . . . . . . . b 5 b . . . . 
-    . . . . . . b b b b b b . . . . 
-    . . . . . b b 5 5 5 5 5 b . . . 
-    . . . . b b 5 b c 5 5 d 4 c . . 
-    . b b b b 5 5 5 b f d d 4 4 4 b 
-    . b d 5 b 5 5 b c b 4 4 4 4 b . 
-    . . b 5 5 b 5 5 5 4 4 4 4 b . . 
-    . . b d 5 5 b 5 5 5 5 5 5 b . . 
-    . b d b 5 5 5 d 5 5 5 5 5 5 b . 
-    b d d c d 5 5 b 5 5 5 5 5 5 b . 
-    c d d d c c b 5 5 5 5 5 5 5 b . 
-    c b d d d d d 5 5 5 5 5 5 5 b . 
-    . c d d d d d d 5 5 5 5 5 d b . 
-    . . c b d d d d d 5 5 5 b b . . 
-    . . . c c c c c c c c b b . . . 
+    ...............ff.......
+    .............ff2ffff....
+    ............ff2feeeeff..
+    ...........ff22feeeeeff.
+    ...........feeeeffeeeef.
+    ..........fe2222eefffff.
+    ..........f2effff222efff
+    ..........fffeeeffffffff
+    ..........fee44fbe44efef
+    ...........feddfb4d4eef.
+    ..........c.eeddd4eeef..
+    ....ccccccceddee2222f...
+    .....dddddcedd44e444f...
+    ......ccccc.eeeefffff...
+    ..........c...ffffffff..
+    ...............ff..fff..
+    ........................
+    ........................
+    ........................
+    ........................
+    ........................
+    ........................
+    ........................
+    ........................
     `, SpriteKind.Player)
 mySprite.setPosition(29, 29)
 locatorSprite = sprites.create(img`
